badcdfd7fffbc662ce40cda0b873d8198c94f3db
jer-lil <35275270+jer-lil@users.noreply.github.com>
2023-12-10 09:49:23 -0800

    deleted old placeholder python files

--- notion_automation/scripts/update_all_TEMP.py
+++
@@ -1,3 +0,0 @@
-if __name__== "__main__":
-  print("Eventually this will update all databases.")
-  print("For now, I just output some text to test the gh actions workflow.")  
--- notion_automation/tests/test_update_all_databases.py
+++
@@ -1,4 +0,0 @@
-if __name__== "__main__":
-  print("Eventually this run a test on update_all_databases \
-        in order to ensure databases are properly updated.")
-  print("For now, I just output some text to test the gh actions workflow.")  
